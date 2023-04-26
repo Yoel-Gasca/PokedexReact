3f48034c3f8a70fa06d475f978eb9606fc794cfa
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2023-04-25 19:35:55 -0600

    Navigation config

--- src/components/Navitagion.jsx
+++ src/components/Navitagion.jsx
@@ -1,11 +1,50 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import logo from '../assets/img/Pokédex_logo.png';
 
 export const Navigation = () => {
     return (
-        <div>
-            <h1>PokeDex</h1>
-            <Outlet/>
-        </div>
+            <>
+                <header className='container'>
+                    <Link to='/' className='logo'>
+                        <img
+                            src='https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png'
+                            alt='Logo Pokedex'
+                        />
+                    </Link>
+
+                    {/*onSubmit={onSearchSubmit}*/}
+                    <form >
+                        <div className='form-group'>
+                            <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                fill='none'
+                                viewBox='0 0 24 24'
+                                strokeWidth='1.5'
+                                stroke='currentColor'
+                                className='icon-search'
+                            >
+                                <path
+                                    strokeLinecap='round'
+                                    strokeLinejoin='round'
+                                    d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+                                />
+                            </svg>
+                            <input
+                                type='search'
+                                name='valueSearch'
+                                id=''
+                                //value={valueSearch}
+                                //onChange={onInputChange}
+                                placeholder='Buscar nombre de pokemon'
+                            />
+                        </div>
+
+                        <button className='btn-search'>Buscar</button>
+                    </form>
+                </header>
+
+                <Outlet/>
+            </>    
     );
 };
