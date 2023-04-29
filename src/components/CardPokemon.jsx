8bd20818d760506a7da901dcc654c38ca96cabce
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2023-04-28 20:39:45 -0600

    TextureCards and Shiny

--- src/components/CardPokemon.jsx
+++ src/components/CardPokemon.jsx
@@ -7,13 +7,32 @@ export const CardPokemon = ({ pokemon }) => {
         <Link to={`/pokemon/${pokemon.id}`} className='card-pokemon'>
 			<div className='card-img'>
 				<img
-					src={pokemon.sprites.other.dream_world.front_default}
+					src={pokemon.sprites.other.home.front_default}
 					alt={`Pokemon ${pokemon.name}`}
 				/>
 			</div>
+			
 			<div className='card-info'>
 				<span className='pokemon-id'>N° {pokemon.id}</span>
 				<h3>{pokemon.name}</h3>
+				<div className='card-types'>
+					{pokemon.types.map(type => (
+						<span key={type.type.name} className={type.type.name}>
+							{type.type.name}
+						</span>
+					))}
+				</div>
+			</div>
+			
+			<div className="card-img2">
+				<img
+					src={pokemon.sprites.other.home.front_shiny}
+					alt={`Pokemon ${pokemon.name}`}
+				/>
+			</div>
+			<div className='card-info2'>
+				<span className='pokemon-id'>N° {pokemon.id}</span>
+				<h3>{pokemon.name}</h3> <h4>★ Shiny</h4>
 				<div className='card-types'>
 					{pokemon.types.map(type => (
 						<span key={type.type.name} className={type.type.name}>
