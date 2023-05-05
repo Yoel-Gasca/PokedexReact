ceef5e87ffe472d4d34f29b6f6a5fa2cfb00f46a
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2023-05-04 20:24:05 -0600

    Aspect changes

--- src/components/CardPokemon.jsx
+++ src/components/CardPokemon.jsx
@@ -24,23 +24,7 @@ export const CardPokemon = ({ pokemon }) => {
 				</div>
 			</div>
 			
-			<div className="card-img2">
-				<img
-					src={pokemon.sprites.other.home.front_shiny}
-					alt={`Pokemon ${pokemon.name}`}
-				/>
-			</div>
-			<div className='card-info2'>
-				<span className='pokemon-id'>N° {pokemon.id}</span>
-				<h3>{pokemon.name}</h3> <h4>★ Shiny</h4>
-				<div className='card-types'>
-					{pokemon.types.map(type => (
-						<span key={type.type.name} className={type.type.name}>
-							{type.type.name}
-						</span>
-					))}
-				</div>
-			</div>
+			
 		</Link>
     );
 };
--- src/pages/PokemonPage.jsx
+++ src/pages/PokemonPage.jsx
@@ -109,83 +109,16 @@ export const PokemonPage = () => {
 					</div>
 					
 
-
 					<div className='header-main-pokemon'>
-						<div className='container-img-pokemon'>
+						<div className='container-info-pokemon-shiny'>
+							<h2>Aspecto Tipo Shiny</h2>
+							<h3>★</h3>
+						</div>
+						<div className='container-img-pokemon-shiny'>
 							<img
 								src={pokemon.sprites.other.home.front_shiny}
 								alt={`Pokemon ${pokemon?.name}`}
 							/>
-						</div>
-
-						<div className='container-info-pokemon'>
-							<h1>{primerMayuscula(pokemon.name)}</h1>
-							<h2>★ Shiny</h2>
-							<div className='card-types info-pokemon-type'>
-								{pokemon.types.map(type => (
-									<span key={type.type.name} className={`${type.type.name}`}>
-										{type.type.name}
-									</span>
-								))}
-							</div>
-							<div className='info-pokemon'>
-								<div className='group-info'>
-									<p>Altura</p>
-									<span>{pokemon.height}</span>
-								</div>
-								<div className='group-info'>
-									<p>Peso</p>
-									<span>{pokemon.weight}KG</span>
-								</div>
-							</div>
-						</div>
-					</div>
-
-					<div className='container-stats'>
-						<h1>Estadísticas</h1>
-						<div className='stats'>
-							<div className='stat-group'>
-								<span>Hp</span>
-								<div className='progress-bar'></div>
-								<span className='counter-stat'>
-									{pokemon.stats[0].base_stat}
-								</span>
-							</div>
-							<div className='stat-group'>
-								<span>Attack</span>
-								<div className='progress-bar'></div>
-								<span className='counter-stat'>
-									{pokemon.stats[1].base_stat}
-								</span>
-							</div>
-							<div className='stat-group'>
-								<span>Defense</span>
-								<div className='progress-bar'></div>
-								<span className='counter-stat'>
-									{pokemon.stats[2].base_stat}
-								</span>
-							</div>
-							<div className='stat-group'>
-								<span>Special Attack</span>
-								<div className='progress-bar'></div>
-								<span className='counter-stat'>
-									{pokemon.stats[3].base_stat}
-								</span>
-							</div>
-							<div className='stat-group'>
-								<span>Special Defense</span>
-								<div className='progress-bar'></div>
-								<span className='counter-stat'>
-									{pokemon.stats[4].base_stat}
-								</span>
-							</div>
-							<div className='stat-group'>
-								<span>Speed</span>
-								<div className='progress-bar'></div>
-								<span className='counter-stat'>
-									{pokemon.stats[5].base_stat}
-								</span>
-							</div>
 						</div>
 					</div>
 
