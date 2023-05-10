415114889a4237af7c33e754613e770e95357416
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2023-05-09 18:12:09 -0600

    Deploy app

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,7 @@ import { AppRouter } from "./AppRouter";
 import { PokemonContext } from "./context/PokemonContext";
 import { PokemonProvider } from "./context/PokemonProvider";
 
-/* INSTRUCCIONES
-  Comandos usados:
--> npm init vite -> inicia un proyecto con vite 
--> Dar un nombre al proyecto y a su paqueteria
--> Seleciona React y lenguaje a utilizar JS o TS (JavaScript)
--> cd '.\NombreCarpeta\'
--> npm i -> instala la paqueteria de React prederteminada
--> npm i react-router-dom@6 -> instala las propiedades para rutas de React
--> npm i @uiball/loaders Instala los estilos de carga
--> npm run dev -> Ejecuta la aplicacion
-*/
+// Funcion general de la aplicacion
 function App() {
   return (
     <PokemonProvider>
